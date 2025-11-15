c2a0874bac439a4e7b45f2eea09635b8ed203873
ossiprut <129540005+ossiprut@users.noreply.github.com>
2025-11-15 23:49:39 +0100

    Add files via upload

---
+++ Content.js
@@ -0,0 +1,15 @@
+let BobRossImages = [
+    "https://bit.ly/3Ck6DTU",
+    "https://bit.ly/3ozQCVk",
+    "https://bit.ly/3omYDN6",
+    "https://bit.ly/3osrfoi",
+    "https://bit.ly/3qCPjax",
+    "https://bit.ly/3CkRXE6",
+];
+
+const imgs = document.getElementsByTagName("img");
+
+for (image of imgs) {
+    const index = Math.floor(Math.random() * BobRossImages.length);
+    image.src = BobRossImages[index];
+}
